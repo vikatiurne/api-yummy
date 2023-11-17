b1751d241f9ec72fc741e118c7fada5c763eefda
vikatiurne <viktoriya83-08@ukr.net>
2023-11-17 14:42:31 +0200

    test db

--- bd.js
+++ bd.js
@@ -2,13 +2,18 @@ import { Sequelize } from 'sequelize';
 import pg from 'pg';
 
 export default new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
+  "yummy_qfbg",
+  "yummy_qfbg_user",
+  "myW6oZiKuEBV4wKkVQTxwrYy1vJiqh70",
   {
     dialect: 'postgres',
     dialectModule: pg,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    host: 'dpg-clatfdu16hkc7380ppm0-a',
+    port: 5432,
   }
 );
