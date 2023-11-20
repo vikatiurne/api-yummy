f78274d1e21bb44f83396275395b11965918790a
vikatiurne <viktoriya83-08@ukr.net>
2023-11-20 12:39:24 +0200

    cors

--- middleware/CorsMiddleware.js
+++ middleware/CorsMiddleware.js
@@ -1,5 +1,5 @@
 export default function allowCrossDomain(req, res, next) {
-  res.header(`Access-Control-Allow-Origin`, `example.com`);
+  res.header(`Access-Control-Allow-Origin`, `https://api-yummy.onrender.com`);
   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
   next();
