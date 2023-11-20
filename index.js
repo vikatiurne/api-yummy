956f1776e5a56c1913a9c1bfc9ce40710d016e93
vikatiurne <viktoriya83-08@ukr.net>
2023-11-20 16:00:48 +0200

    cors

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    credentials: true,
+    credentials: false,
+    // credentials: true,
     origin: "*",
     // origin: process.env.CLIENT_URL,
   })
