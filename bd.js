f587aca26b2134d63539985ed14b8ed1c723164c
vikatiurne <viktoriya83-08@ukr.net>
2023-11-21 13:16:50 +0200

    auth google

--- bd.js
+++ bd.js
@@ -2,17 +2,24 @@ import { Sequelize } from 'sequelize';
 import pg from 'pg';
 
 export default new Sequelize(
+  // local:
   // process.env.DB_NAME,
   // process.env.DB_USER,
   // process.env.DB_PASSWORD,
-  "yummy_qfbg",
-  "yummy_qfbg_user",
-  "myW6oZiKuEBV4wKkVQTxwrYy1vJiqh70",
+
+  // prod:
+  'yummy_qfbg',
+  'yummy_qfbg_user',
+  'myW6oZiKuEBV4wKkVQTxwrYy1vJiqh70',
   {
     dialect: 'postgres',
     dialectModule: pg,
+
+    // local:
     // host: process.env.DB_HOST,
     // port: process.env.DB_PORT,
+
+    // prod:
     host: 'dpg-clatfdu16hkc7380ppm0-a',
     port: 5432,
   }
--- controllers/googleAuthController.js
+++ controllers/googleAuthController.js
@@ -28,9 +28,14 @@ class GoogleAuthController {
     const { id_token, access_token } = await googleOAuthService.getGoogleTokens(
       {
         code,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        // prod:
+        clientId: "1073086270333-c5h4tukvqvdtcd28cl6j8qvf9d7lnmfh.apps.googleusercontent.com",
+        clientSecret: "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",
         redirectUri: `https://api-yummy.onrender.com/api/user/auth/google`,
+
+        // local:
+        // clientId: process.env.GOOGLE_CLIENT_ID,
+        // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         // redirectUri: `${process.env.API_URL}/api/user/auth/google`,
       }
     );
