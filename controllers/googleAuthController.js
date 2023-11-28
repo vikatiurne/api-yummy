03b6def77acf52954e0f922e078cb30412bdaa1c
vikatiurne <viktoriya83-08@ukr.net>
2023-11-28 09:48:52 +0200

    test

--- controllers/googleAuthController.js
+++ controllers/googleAuthController.js
@@ -60,6 +60,7 @@ class GoogleAuthController {
   async getCurentGoogleUser(req, res, next) {
     try {
       const refreshToken = await req.cookies['refreshToken'];
+      console.log(refreshToken)
       if (refreshToken) {
         const userData = tokenService.validateRefreshToken(refreshToken);
         const email = await userData.email;
